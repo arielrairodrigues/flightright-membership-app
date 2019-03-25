e4265c6e81ed6bc9369ddbc355ce6159f2a4c75e
Megafu <megafu.charles@yahoo.com>
2019-03-25 20:30:31 +0100

    Completed test cases for MemberService

--- flightRightService/src/test/java/com/flightright/service/MemberServiceTest.java
+++ flightRightService/src/test/java/com/flightright/service/MemberServiceTest.java
@@ -9,14 +9,20 @@ import com.flightright.persistence.model.Member;
 import com.flightright.persistence.repo.MemberRepo;
 import com.flightright.service.impl.MemberServiceImpl;
 import static com.flightright.service.util.Util.mockMember;
+import static com.flightright.service.util.Util.mockMembers;
+import java.util.List;
 import java.util.Optional;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -64,5 +70,36 @@ public class MemberServiceTest {
         assertEquals(member.getLastName(), mockMember().getLastName());
         assertThat(member.getPicture(), is(mockMember().getPicture()));
         verify(memberRepo, times(1)).findById(any());
+    }
+    
+    /**
+     * Test case for delete member
+     * @throws Exception 
+     */
+    @Test
+    public void when_deleteMember_thenDoNothing() throws Exception {
+        doNothing().when(memberRepo).deleteById(any());
+        // make service call
+        memberService.deleteMember(1L);
+        verify(memberRepo, times(1)).deleteById(any());
+    }
+    
+    /**
+     * Test case to get all members
+     * @throws Exception 
+     */
+    @Test
+    public void when_findAll_thenReturnList() throws Exception {
+        when(memberRepo.findAll()).thenReturn(mockMembers());
+        
+        // make the service call
+        List<Member> members = memberService.findAll();
+        
+        assertNotNull(members);
+        assertFalse(members.isEmpty());
+        assertThat(members, containsInAnyOrder( // confirm that each of the elements returned the appropriate expected attributes
+                hasProperty("firstName", is("Nonso"))
+                ));
+        assertThat(members.size(), is(mockMembers().size())); // confirm that the sizes are the same
     }
 }
--- flightRightService/src/test/java/com/flightright/service/util/Util.java
+++ flightRightService/src/test/java/com/flightright/service/util/Util.java
@@ -6,6 +6,8 @@
 package com.flightright.service.util;
 
 import com.flightright.persistence.model.Member;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -15,9 +17,24 @@ public final class Util {
     
     private Util() {}
     
+    /**
+     * Mock Member
+     * @return 
+     */
     public static Member mockMember() {
         return new Member.Builder().setFirstName("Nonso")
                                    .setPicture("cert.jpg")
                                    .setLastName("Megafu").build();
+    }
+    
+    /**
+     * Mock the list of members
+     * @return 
+     */
+    public static List<Member> mockMembers() {
+        Member member = new Member(1L, "Nonso", "Megafu", null, "100212", "picture.jgp", null, null);
+        List<Member> members = new ArrayList<>();
+        members.add(member);
+        return members;
     }
 }
